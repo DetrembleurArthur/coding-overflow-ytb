d1eb97c9e6b59ff2e8e6b19a2c0a76785c0f91ff
ArthurDetrembleur <arthur.detrembleur@student.hepl.be>
2023-01-31 14:26:31 +0100

    update pentes function

--- ia/utils/derivee.py
+++ ia/utils/derivee.py
@@ -6,18 +6,20 @@ import math
 def f(x):
     return 1/math.sqrt(2*math.pi) * (math.e**(-0.5 * x**2))
 
-def pentes(x, y):
-    p = []
-    for i in range(len(x) - 1):
-        pente = (y[i+1]-y[i])/(x[i+1]-x[i])
-        p.append(pente)
-    p.append(p[len(p)-1])
-    return p
+def pentes(x, f, step=0.01):
+    acc = []
+    for i in range(len(x)):
+        x1 = x[i] - step
+        x2 = x[i] + step
+        y1 = f(x1)
+        y2 = f(x2)
+        pente = (y2-y1)/(x2-x1)
+        acc.append(pente)
+    return acc
 
 x = np.linspace(-5.0, 5.0, num=10000)
 y = np.array(list(map(f, x)))
 plt.plot(x, y, label="function", c="blue")
-plt.plot(x, pentes(x, y), label="derivee", c="red")
-plt.plot(x, pentes(x, pentes(x, y)), label="derivee", c="orange")
+plt.plot(x, pentes(x, f), label="derivee", c="red")
 plt.axhline(y = 0, color = 'green')
 plt.show()
